734cc961daae807f98585cf481980ceb0e5d3715
Cesar Ivann Llamas <ivllamoficial@gmail.com>
2023-01-01 21:38:10 -0600

    change from client to bot, and write some comments

--- 001-newBorn.py
+++ 001-newBorn.py
@@ -1,30 +1,41 @@
+#Import the libraries
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 import os
 
+# defining the prefix
+prefix = '$'
+# Charge all the .env files to read the TOKEN for our bot
 load_dotenv()
+# getting the token
 TOKEN = os.getenv('TOKEN')
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents=intents)
+# defining the bot with the prefix
+bot = commands.Bot(command_prefix=prefix, intents=intents)
+# removing the command help to stablish one custome
+bot.remove_command('help')
 
-@client.event
+# --------------- When the bot is ready -----------------------
+@bot.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    print(f'Logged in {bot.user}')
 
-@client.event
+
+@bot.event
 async def on_message(message):
-    if message.author == client.user:
+    if message.author == bot.user:
         return
     
     if message.content.startswith('hello'):
         await message.channel.send('Hello!')
 
-@client.event
+@bot.event
 async def on_member_join(member):
     privateChannel = await member.create_dm()
     await privateChannel.send(f'Hey {member} I am 001-New Born, I\'m a simple bot, I\'m here to help you in all things you need in the future.')
     await privateChannel.send(f'Actually I\'m a bot of the serie New Born, I\'m not only bots of discord, In the future my creators are planning to crete more, so enjoy the channel')
 
-client.run(TOKEN)
+bot.run(TOKEN)
